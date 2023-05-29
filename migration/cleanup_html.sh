c024eb5f59876ab0a0b3a032cfa0cfe61e247261
Timid Robot Zehta <timid@creativecommons.org>
2023-05-29 08:30:55 -0700

    dedicated to the public domain

--- migration/cleanup_html.sh
+++ migration/cleanup_html.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Dedicated to the public domain under the CC0 1.0 Universal (CC0 1.0) Public
+# Domain Dedication: https://creativecommons.org/publicdomain/zero/1.0/
 set -o errexit
 set -o errtrace
 set -o nounset
